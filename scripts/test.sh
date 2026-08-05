943a8fb1d8d84084b96f1418a96a7f4e6a4f2858
Jay Kalinani <jaykalinani@gmail.com>
2026-08-05 00:01:43 -0500

    CI: Select nuX arrangement tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -13,7 +13,7 @@ export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 time ./simfactory/bin/sim \
     --machine="actions-$ACCELERATOR-$REAL_PRECISION" \
     create-run NuXTest01 --cores 1 --num-threads 2 \
-    --testsuite --select-tests=nuX_M1
+    --testsuite --select-tests=nuX
 
 TEST_OUTPUT_DIR="$(./simfactory/bin/sim \
     --machine="actions-$ACCELERATOR-$REAL_PRECISION" \
