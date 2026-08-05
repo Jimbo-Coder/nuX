88986b59c6f4a1aaa13648e68fee481abc796e8b
Jay Kalinani <jaykalinani@gmail.com>
2026-08-04 21:25:29 -0500

    CI: Make nuX workflow self-contained

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,25 +6,17 @@ export NUXSPACE="$PWD"
 export WORKSPACE="$PWD/../workspace"
 cd "$WORKSPACE/Cactus"
 
-ASTERX_SCRIPTS="$WORKSPACE/Cactus/arrangements/AsterX/scripts"
-cp "$ASTERX_SCRIPTS/actions-$ACCELERATOR-$REAL_PRECISION.cfg" \
+NUX_SCRIPTS="$NUXSPACE/scripts"
+cp "$NUX_SCRIPTS/actions-$ACCELERATOR-$REAL_PRECISION.cfg" \
     simfactory/mdb/optionlists/
-cp "$ASTERX_SCRIPTS/actions-$ACCELERATOR-$REAL_PRECISION.ini" \
+cp "$NUX_SCRIPTS/actions-$ACCELERATOR-$REAL_PRECISION.ini" \
     simfactory/mdb/machines/
-cp "$ASTERX_SCRIPTS/actions-$ACCELERATOR-$REAL_PRECISION.run" \
+cp "$NUX_SCRIPTS/actions-$ACCELERATOR-$REAL_PRECISION.run" \
     simfactory/mdb/runscripts/
-cp "$ASTERX_SCRIPTS/actions-$ACCELERATOR-$REAL_PRECISION.sub" \
+cp "$NUX_SCRIPTS/actions-$ACCELERATOR-$REAL_PRECISION.sub" \
     simfactory/mdb/submitscripts/
-cp "$ASTERX_SCRIPTS/defs.local.ini" simfactory/etc/
-cp "$NUXSPACE/scripts/nux.th" .
-
-sed -i \
-    -e 's#/__w/AsterX/workspace#/__w/nuX/workspace#g' \
-    -e 's#AsterX CI#nuX CI#g' \
-    -e 's#github.com/jaykalinani/AsterX#github.com/jaykalinani/nuX#g' \
-    "simfactory/mdb/machines/actions-$ACCELERATOR-$REAL_PRECISION.ini"
-sed -i -e 's/thornlist       = asterx.th/thornlist       = nux.th/' \
-    simfactory/etc/defs.local.ini
+cp "$NUX_SCRIPTS/defs.local.ini" simfactory/etc/
+cp "$NUX_SCRIPTS/nux.th" .
 
 if command -v ccache >/dev/null 2>&1; then
     export CCACHE_DIR="${CCACHE_DIR:-$NUXSPACE/.ccache}"
